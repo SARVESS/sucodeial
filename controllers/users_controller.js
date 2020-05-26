d086fb10d601f1bd683be94f66a74625c2cfa852
SARVESS <sarveshsri76@gmail.com>
2020-05-27 03:04:20 +0530

    sign-up and sign-in page rendered and forms added to it

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,29 @@ module.exports.profile = function(req, res){
        user_name: "Sarvesh Srivastava"
    });
 }
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Sucodeial | Sign Up"
+    });
+}
+
+//render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Sucodeial | Sign In"
+    });
+}
+
+//get the sign up data
+module.exports.create = function(req, res){
+    //TODO later
+}
+
+//sign in 
+module.exports.createSession = function(req,res){
+    //TODO later
+}
+
+
