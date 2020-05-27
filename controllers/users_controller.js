989ec3193bd188caf00d6944ecb4ea552ea67630
SARVESS <sarveshsri76@gmail.com>
2020-05-27 05:42:32 +0530

    Manual Authentication Using create-session(sign-in) and sign-out done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,20 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-   return res.render('user_profile',{
-       title: "User",
-       user_name: "Sarvesh Srivastava"
-   });
+   if(req.cookies.user_id){
+      User.findById(req.cookies.user_id, function(err, user){
+        if(user){
+           return res.render('user_profile',{
+              title: "User Profile",
+              user: user
+           });
+        }
+
+        return res.redirect('/users/sign-in');
+      });
+   }else{
+       return res.redirect('/users/sign-in');
+   }
 }
 
 // render the sign up page
@@ -44,8 +54,36 @@ module.exports.create = function(req, res){
 }
 
 //sign in and create a sesion for the user
-module.exports.createSession = function(req,res){
-    //TODO later
+module.exports.createSession = function(req, res){
+     //steps to authenticate
+     // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing in'); return}
+        // handle user found
+
+        if(user){
+
+           // handle password which doesn't match
+           if(user.password != req.body.password){
+               return res.redirect('back');
+           }
+          // handle session creation 
+          res.cookie('user_id', user.id);
+          return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found 
+
+            return req.redirect('back');
+        }
+    });
+
+}
+// sign out the user
+module.exports.signOut = function(req, res){
+
+    res.clearCookie('user_id');
+    return res.redirect('/users/sign-in');
 }
 
 
