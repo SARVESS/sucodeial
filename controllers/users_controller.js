cbb3fda7d29ff0b4533a314c5e4b4a7954b399bf
SARVESS <sarveshsri76@gmail.com>
2020-05-28 12:18:54 +0530

    Authenticated Using Passport.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,6 +9,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Sucodeial | Sign Up"
     });
@@ -16,6 +19,9 @@ module.exports.signUp = function(req, res){
 
 //render the sign in page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: "Sucodeial | Sign In"
     });
@@ -43,9 +49,14 @@ module.exports.create = function(req, res){
 
 }
 
-//sign in and create a sesion for the user
+// sign in and create a sesion for the user
 module.exports.createSession = function(req,res){
-    //TODO later
+    return res.redirect('/');
 }
 
+// sign-out 
+module.exports.destroySession = function(req, res){
+    req.logout(); 
 
+    return res.redirect('/');
+}
