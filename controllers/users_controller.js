06ff7048a53c02c9a2ec360067d87de80142d1ac
SARVESS <sarveshsri76@gmail.com>
2020-06-17 23:36:35 +0530

    Converting to async await and flash messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 
+
 module.exports.profile = function(req, res){
     User.findById(req.params.id, function(err, user){
         return res.render('user_profile',{
@@ -13,9 +14,11 @@ module.exports.profile = function(req, res){
 module.exports.update = function(req, res){
     if(req.user.id == req.params.id){
         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            req.flash('success', 'Updated Successfully!')
             return res.redirect('back');
         });
     } else {
+        req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
 }
@@ -43,6 +46,7 @@ module.exports.signIn = function(req, res){
 //get the sign up data
 module.exports.create = function(req, res){
     if(req.body.password != req.body.confirm_password){
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
     User.findOne({email:req.body.email}, function(err, user){
@@ -50,11 +54,12 @@ module.exports.create = function(req, res){
 
         if(!user){
             User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
+                if(err){req.flash('error', err); return}
                 
                 return res.redirect('/users/sign-in');
             })
         }else{
+            req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
 
@@ -64,12 +69,14 @@ module.exports.create = function(req, res){
 
 // sign in and create a sesion for the user
 module.exports.createSession = function(req,res){
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 // sign-out 
 module.exports.destroySession = function(req, res){
     req.logout(); 
+    req.flash('success', 'You have logged out!');
 
     return res.redirect('/');
 }
