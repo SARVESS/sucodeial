523e94bd6160fe0b1056b32b72c087b7a998c1b7
SARVESS <sarveshsri76@gmail.com>
2020-06-26 01:20:11 +0530

    create routes for SignIn/SignUp using google

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -3,6 +3,7 @@ const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
 
 const User = require('../models/user');
+const { access } = require('fs');
 
 // tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
@@ -15,7 +16,7 @@ passport.use(new googleStrategy({
         // find a user
         User.findOne({email: profile.emails[0].value}).exec(function(err, user){
             if(err){ console.log('error in google strategy-passport', err); return; }
-
+            // console.log(accessToken, refreshToken);
             console.log(profile);
             
             if(user){
